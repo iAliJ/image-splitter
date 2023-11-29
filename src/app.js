ffbdfe904ede25d54be644037110f1780baf6727
alij <alimjahrami@gmail.com>
2023-11-29 22:08:31 +0300

    refactor

---
+++ src/app.js
@@ -0,0 +1,43 @@
+const sharp = require('sharp');
+
+cropImage('./img/underwater.png');
+
+async function getMetaData() {
+    const metadata = await sharp('./img/underwater.png').metadata();
+    return metadata;
+}
+
+async function cropImage(imagePath) {
+    // Get the width and height of the image
+    const metadata = await getMetaData();
+    const width = metadata.width;
+    const height = metadata.height;
+    // we will split it in 2x2 for now
+    const nx = 2;
+    const ny = 2;
+    const pixelX = width / nx;
+    const pixelY = height / ny;
+    // offset will increase by amount of pixels each iteration
+    let topOffset = 0;
+    let leftOffset = 0;
+    let count = 0;
+
+    while(topOffset != height && leftOffset != width) {
+        await sharp(imagePath)
+        .extract(
+            {
+                left: leftOffset, top: topOffset, width: pixelX, height:pixelY
+            }
+        )
+        .toFile(`./output/underwater-${count}.png`);        
+
+        leftOffset += pixelX;
+        // if we reach edge of the image, go to the next row
+        if(leftOffset == width) {
+            leftOffset = 0;
+            topOffset += pixelY;
+        }
+
+        count++;
+    }
+}
